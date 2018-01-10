df9a34901b0f87677d28d6f64e29dc613c79dafb
Jasper-1024 <ljy087621@gmail.com>
2018-01-10 22:44:41 +0800

    删减fragmentmodel

--- app/src/main/java/com/jasperhale/myprivacy/Activity/adapter/BindAdapter_applist.java
+++ app/src/main/java/com/jasperhale/myprivacy/Activity/adapter/BindAdapter_applist.java
@@ -6,7 +6,6 @@ import android.databinding.ObservableList;
 import android.databinding.ViewDataBinding;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.util.DiffUtil;
